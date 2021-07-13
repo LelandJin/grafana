d218c0dc9e1867a2c02306e11474683bd5b999fe
lelandjin <lnjsz@qq.com>
2021-07-13 13:46:28 +0800

    重布局了文件夹

--- cn_code/pkg/tsdb/graphite/types.go
+++
@@ -1,9 +0,0 @@
-package graphite
-
-import "github.com/grafana/grafana/pkg/plugins"
-
-type TargetResponseDTO struct {
-	Target     string                       `json:"target"`
-	DataPoints plugins.DataTimeSeriesPoints `json:"datapoints"`
-	Tags       map[string]string            `json:"tags"`
-}
